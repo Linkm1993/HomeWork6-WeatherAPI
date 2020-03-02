48ef27c555f18ec52936f9319b8aab73afc758a4
Matt Link <Linkm1993@gmail.com>
2020-03-02 01:41:22 -0500

    Pointers are correct, need to add logic

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let apiKey = "3d0cb4385570239c9c1d2ece1b6d5977"
+let cityName = []
+let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid="+apiKey
+let cityDisplay = $(".citydisplay")
+let weatherDisplay = $(".weatherdisplay")
+//let form = $("#input").val()
+let search = $(".btn")
+let historyDisplay = $(".searchhistory")
+
+
+
+$(search).click(function() {
+    console.log($("input").val());
+    //Take text input push to array
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    
+    
+    
+    
+        console.log(response);
+        console.log(cityName)
+      })
+})
+
+
