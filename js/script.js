909d6a81b324bdad3e65deef78e5da31eabd9b29
Matt Link <Linkm1993@gmail.com>
2020-03-03 00:53:08 -0500

    Fixed queryURL to properly pull from input in form

--- js/script.js
+++ js/script.js
@@ -1,30 +1,24 @@
 let apiKey = "3d0cb4385570239c9c1d2ece1b6d5977"
 let cityName = []
-let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid="+apiKey
+let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey
 let cityDisplay = $(".citydisplay")
 let weatherDisplay = $(".weatherdisplay")
 //let form = $("#input").val()
 let search = $(".btn")
 let historyDisplay = $(".searchhistory")
 
-
+function input(){
+  let cityInput = $("input").val()
+  cityName = cityInput
+  console.log(cityName)
+}
 
 $(search).click(function() {
-    let cityInput = $("input").val()
-    cityName.push(cityInput)
-    console.log(cityInput);
-    //Take text input push to array
     $.ajax({
-        url: queryURL,
+        beforeSend: input(),
+        url: "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey,
         method: "GET"
       }).then(function(response) {
-    
-    
-    
-    
         console.log(response);
-        console.log(cityName)
       })
 })
-
-
