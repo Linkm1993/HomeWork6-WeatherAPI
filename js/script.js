8b7aa63f79833833d43f1f766cbd138ad641b903
Matt Link <Linkm1993@gmail.com>
2020-03-03 20:24:49 -0500

    Displaying date, need to set up showing dates for the following days

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ let cityName = []
 let forcast = []
 let tempArray = []
 let iconArray = []
+let dateArray = []
 let cityDisplay = $(".citydisplay")
 let weatherDisplay = $(".weatherdisplay")
 let search = $(".search")
 let clear = $(".clear")
 let historyDisplay = $(".searchhistory")
+// let m = moment()
+
 
 //Setting cityInput to the value of the form, setting cityName to the same value as city input
 function input(){
@@ -47,28 +50,36 @@ $(search).click(function() {
         let weatherDescription = response.list[i].weather[0].description
         //Grabs weather icon
         let weatherIcon = response.list[i].weather[0].icon
+        //grabs current day
+        let currentDate = moment().format("dddd, MMMM Do YYYY")
 
         //Creating new divs to hold the seperate divs for the 5 day forcast
         let container = $("<div></div>")
         let forcastDisplay =$("<div></div>")
         let weather = $("<div></div>")
+        let date = $("<div></div>")
 
         //Appending new divs
         weatherDisplay.append(container)
+        container.append(date)
         container.append(forcastDisplay)
         container.append(weather)
+
 
         //Adding a class to the new divs
         $(container).addClass("container col-md")
         $(forcastDisplay).addClass("col-md temp")
         $(weather).addClass("col-md discrip")
+        $(date).addClass("col-md date")
 
         //Pushing data to each spefic array
         forcast.push(weatherDescription)
         tempArray.push(temps)
         iconArray.push(weatherIcon)
+        dateArray.push(currentDate)
         
         //Prints the 5 day forcast
+        $(date).html(dateArray[i])
         $(forcastDisplay).html(tempArray[i])
         $(weather).html(forcast[i])
       }
@@ -89,8 +100,13 @@ $(search).click(function() {
 
 
 
+
       console.log(forcast)
       console.log(tempArray)
+      console.log(dateArray)
+      // console.log()
+      // console.log((m.add(1, "days")))
+      // console.log(m._d)      
       // console.table(response.list)
       // console.table(response.list[0].weather)
       })
